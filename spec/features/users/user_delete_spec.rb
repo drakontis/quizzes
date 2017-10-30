0f45b310e36448c3db5f3576b0ffb1ae1252d643
Drakontis Panagiotis <drakontis.panagiotis@gmail.com>
2017-10-30 16:07:43 +0200

    Fix and add pending test.

--- spec/features/users/user_delete_spec.rb
+++ spec/features/users/user_delete_spec.rb
@@ -16,15 +16,13 @@ RSpec.feature 'User delete', :devise, :js do
   #   When I delete my account
   #   Then I should see an account deleted message
   scenario 'user can delete own account' do
-    skip 'skip a slow test'
     user = FactoryGirl.create(:user)
     login_as(user, :scope => :user)
     visit edit_user_registration_path(user)
     click_button 'Cancel my account'
-    page.driver.browser.switch_to.alert.accept
+    page.execute_script 'window.confirm = function () { return true }'
     expect(page).to have_content I18n.t 'devise.registrations.destroyed'
   end
-
 end
 
 
